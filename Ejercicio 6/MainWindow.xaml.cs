d37f9ed1b4d674a59db47df6acd61989f910a72e
Maria <Maria@DESKTOP-G07TP2O>
2020-10-20 17:18:01 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio 6/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace Ejercicio_6
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Imagen_MouseMove(object sender, MouseEventArgs e)
+        {
+            Image imagen = (Image)sender;
+            imagen.Opacity = 1;
+        }
+        private void Imagen_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image imagen = (Image)sender;
+            imagen.Opacity = 0.5;
+        }
+    }
+}
